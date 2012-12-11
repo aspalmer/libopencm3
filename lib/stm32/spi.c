04c1b953888e724d2d3e5e352d6d0c9c1b33d6b5
austin <austin@austin-linux.(none)>
2012-12-11 11:16:09 -0800

    Replaced if with while loop for i2s_disable

--- lib/stm32/spi.c
+++ lib/stm32/spi.c
@@ -232,18 +232,15 @@ void i2s_enable(u32 spi)
  * @param[in] spi Unsigned int32. SPI peripheral identifier @ref spi_reg_base.
  * */
 
-uint8_t i2s_disable(u32 spi)
-{
-	u32 reg;
-	reg = SPI_SR(spi);
+void i2s_disable(u32 spi)
+
+	u32 reg = SPI_SR(spi);
 	/* it is mandatory to wait for TXE = 1 and BSY = 0 */
-	if ((reg & SPI_SR_TXE)&&((reg & SPI_SR_BSY) == 0)) {
-		reg = SPI_I2SCFGR(spi);
-		reg &= 0b011111111111;
-		SPI_I2SCFGR(spi) = reg;
-		return 1;
-	}
-	return 0;
+	while ((reg & SPI_SR_TXE)&&((reg & SPI_SR_BSY) != 0));
+
+	reg = SPI_I2SCFGR(spi);
+	reg &= 0b011111111111;
+	SPI_I2SCFGR(spi) = reg;
 }
 
 /* TODO: Error handling? */
